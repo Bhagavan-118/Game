fb26512383717323f3c4845e13815c57509302bd
Bhagavan-118 <Venkata.b.namburi@gmail.com>
2024-12-22 01:23:34 +0530

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,69 @@
+// function lowerCaseString(str1,str2){
+//     let answer;
+// // You have been provided with two strings
+// Str1="JOHN"
+// Str2 = "DOE"
+// //"You need to convert str1 and str2 to lowercase, concatenate them together, and store the concatenated string in the variable 'answer'."
+//              answer = str1.toLowerCase() + str2.toLowerCase()
+//     	return answer;
+// }
+// function isEligible(mathScore , englishScore , scienceScore) {
+//     let eligibleForAward;
+//     let eligibleForScholarship = (mathScore > 80 && englishScore > 80) ||
+//                 (mathScore > 80 && scienceScore > 80) ||
+//                 (englishScore > 80 && scienceScore > 80);
+
+// 50
+// The student is eligible for an award if they qualify for either a scholarship or a science fair.
+//  eligibleForAward = eligibleForScholarship || qualifiesForScienceFair;
+// return eligibleForAward
+
+// }
+// let englishScore=77;
+// let mathScore=84;
+// let scienceScore=93;
+
+// let eligible = isEligible(mathScore , englishScore , scienceScore);
+// result(eligible);
+  // let result;
+  // if (result >= 90) {
+  //   console.log("A grade");
+  // } else if (result >= 80 && result <= 89) {
+  //   console.log("B grade");
+  // } else if (result >= 70 && result <= 79) {
+  //   console.log("C grade");
+  // } else if (result >= 60 && result <= 69) {
+  //   console.log("D grade");
+  // } else if (result < 60) {
+  //   console.log("F grade");
+  // }
+
+
+// let num = 50;
+// if (num % 2 == 0) {
+//   console.log("Even number"+" "+(num * num) );
+// }else{
+//   console.log("Odd number")
+//  }
+
+// const year = 198000;
+
+// if (year % 4 === 0) {
+//   if (year % 100 === 0) {
+//     if (year % 400 === 0) {
+//       console.log("It's a leap year!");
+//     } else {
+//       console.log("It's not a leap year.");
+//     }
+//   } else {
+//     console.log("It's a leap year!");
+//   }
+// } else {
+//   console.log("It's not a leap year.");
+// }
+  
+let x=10;
+let y=10;
+document.write(
+x==y ?"equal":"False"
+)
